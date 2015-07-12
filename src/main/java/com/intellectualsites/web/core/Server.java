f957fe22114b0163ab83ca086186df0a0053637c
Sauilitired <citymonstret@hotmail.com>
2015-07-12 17:02:38 +0200

    Fully comply with GNU GPLv2

--- src/main/java/com/intellectualsites/web/core/Server.java
+++ src/main/java/com/intellectualsites/web/core/Server.java
@@ -1,3 +1,22 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//     IntellectualServer is a web server, written entirely in the Java language.                            /
+//     Copyright (C) 2015 IntellectualSites                                                                  /
+//                                                                                                           /
+//     This program is free software; you can redistribute it and/or modify                                  /
+//     it under the terms of the GNU General Public License as published by                                  /
+//     the Free Software Foundation; either version 2 of the License, or                                     /
+//     (at your option) any later version.                                                                   /
+//                                                                                                           /
+//     This program is distributed in the hope that it will be useful,                                       /
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of                                        /
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                                         /
+//     GNU General Public License for more details.                                                          /
+//                                                                                                           /
+//     You should have received a copy of the GNU General Public License along                               /
+//     with this program; if not, write to the Free Software Foundation, Inc.,                               /
+//     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.                                           /
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 package com.intellectualsites.web.core;
 
 import com.intellectualsites.web.config.ConfigVariableProvider;
@@ -97,7 +116,7 @@ public class Server extends Thread implements IntellectualServer {
     private final boolean standalone;
     private final int port;
     private final Map<String, Class<? extends View>> viewBindings;
-    private final LogWrapper logWrapper;
+    public final LogWrapper logWrapper;
 
     {
         viewBindings = new HashMap<>();
@@ -118,6 +137,19 @@ public class Server extends Thread implements IntellectualServer {
         instance = this;
 
         Assert.notNull(coreFolder, logWrapper);
+
+        {
+            logWrapper.log("");
+            logWrapper.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+            logWrapper.log("> GNU GENERAL PUBLIC LICENSE NOTICE:");
+            logWrapper.log("> ");
+            logWrapper.log("> IntellectualServer, Copyright (C) 2015 IntellectualSites");
+            logWrapper.log("> IntellectualSites comes with ABSOLUTELY NO WARRANTY; for details type `/show w`");
+            logWrapper.log("> This is free software, and you are welcome to redistribute it");
+            logWrapper.log("> under certain conditions; type `/show c` for details.");
+            logWrapper.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+            logWrapper.log("");
+        }
 
         this.logWrapper = logWrapper;
         this.standalone = standalone;
