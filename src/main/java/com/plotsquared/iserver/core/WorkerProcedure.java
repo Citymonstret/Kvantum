eded1420e29411ac11e1178e3d9588c375231f7b
Sauilitired <citymonstret@hotmail.com>
2016-08-15 23:49:29 +0200

    Documentation ;D

--- src/main/java/com/plotsquared/iserver/core/WorkerProcedure.java
+++ src/main/java/com/plotsquared/iserver/core/WorkerProcedure.java
@@ -1,60 +1,48 @@
 package com.plotsquared.iserver.core;
 
 import com.plotsquared.iserver.object.Request;
+import com.plotsquared.iserver.util.Assert;
 import com.plotsquared.iserver.views.RequestHandler;
 
 import java.lang.ref.WeakReference;
 import java.util.*;
 
-public class WorkerProcedure
+@SuppressWarnings( "unused" )
+public final class WorkerProcedure
 {
+
+    private final List<WeakReference<WorkerProcedureInstance>> instances = new ArrayList<>();
+    private volatile Map<String, Handler> handlers = new LinkedHashMap<>();
 
     WorkerProcedure()
     {
 
     }
 
-    interface Handler<T>
-    {
-
-        T act(RequestHandler requestHandler, Request request, T in);
-
-        Class<T> getType();
-
-    }
-
-    public static abstract class ByteHandler implements Handler<Byte[]>
-    {
-
-        @Override
-        public Class<Byte[]> getType()
-        {
-            return Byte[].class;
-        }
-    }
-
-    public static abstract class StringHandler implements Handler<String>
-    {
-
-        @Override
-        public final Class<String> getType()
-        {
-            return String.class;
-        }
-
-    }
-
-    private volatile Map<String, Handler> handlers = new LinkedHashMap<>();
-    private final List<WeakReference<WorkerProcedureInstance>> instances = new ArrayList<>();
-
+    /**
+     * Add a procedure, and allow the Worker to use it
+     * @param name Procedure name
+     * @param handler Procedure Handler
+     */
     public synchronized void addProcedure(final String name, final Handler handler)
     {
         this.handlers.put( name, handler );
         this.setChanged();
     }
 
+    /**
+     * Add a procedure before another procedure. If there is no procedure with the specified name, it will be
+     * added to the last position of the queue
+     * @param name Procedure Name
+     * @param before Name of procedure that the current procedure should be placed before
+     * @param handler Procedure handler
+     */
     public synchronized void addProcedureBefore(final String name, final String before, final Handler handler)
     {
+        Assert.notEmpty( name );
+        Assert.notEmpty( before );
+        Assert.notNull( handler );
+
         final Map<String, Handler> temporaryMap = new HashMap<>( );
         for ( final Map.Entry<String, Handler> entry : handlers.entrySet() )
         {
@@ -72,8 +60,19 @@ public class WorkerProcedure
         this.setChanged();
     }
 
+    /**
+     * Add a procedure after another procedure. If there is no procedure with the specified name, it will be
+     * added to the last position of the queue
+     * @param name Procedure Name
+     * @param after Name of procedure that the current procedure should be placed behind
+     * @param handler Procedure handler
+     */
     public synchronized void addProcedureAfter(final String name, final String after, final Handler handler)
     {
+        Assert.notEmpty( name );
+        Assert.notEmpty( after );
+        Assert.notNull( handler );
+
         final Map<String, Handler> temporaryMap = new HashMap<>( );
         for ( final Map.Entry<String, Handler> entry : handlers.entrySet() )
         {
@@ -118,7 +117,51 @@ public class WorkerProcedure
         }
     }
 
-    public class WorkerProcedureInstance
+    /**
+     * @return A new WorkerProcedureInstance
+     */
+    public final WorkerProcedureInstance getInstance()
+    {
+        return new WorkerProcedureInstance();
+    }
+
+    interface Handler<T>
+    {
+
+        T act(RequestHandler requestHandler, Request request, T in);
+
+        Class<T> getType();
+
+    }
+
+    public static abstract class ByteHandler implements Handler<Byte[]>
+    {
+
+        @Override
+        public Class<Byte[]> getType()
+        {
+            return Byte[].class;
+        }
+    }
+
+    public static abstract class StringHandler implements Handler<String>
+    {
+
+        @Override
+        public final Class<String> getType()
+        {
+            return String.class;
+        }
+
+    }
+
+    /**
+     * An instance containing the handlers from the WorkerProcedure, that
+     * are split into Byte & String Handlers (to make them easier to use in the worker)
+     *
+     * The instance gets updated automatically when the WorkerProcedure is updated
+     */
+    class WorkerProcedureInstance
     {
 
         private volatile Collection<StringHandler> stringHandlers = new ArrayList<>();
@@ -139,20 +182,15 @@ public class WorkerProcedure
             }
         }
 
-        public Collection<StringHandler> getStringHandlers()
+        Collection<StringHandler> getStringHandlers()
         {
             return this.stringHandlers;
         }
 
-        public Collection<ByteHandler> getByteHandlers()
+        Collection<ByteHandler> getByteHandlers()
         {
             return this.byteHandlers;
         }
-    }
-
-    public final WorkerProcedureInstance getInstance()
-    {
-        return new WorkerProcedureInstance();
     }
 
 }
--- src/main/java/com/plotsquared/iserver/views/requesthandler/Middleware.java
+++ src/main/java/com/plotsquared/iserver/views/requesthandler/Middleware.java
@@ -2,6 +2,93 @@ package com.plotsquared.iserver.views.requesthandler;
 
 import com.plotsquared.iserver.object.Request;
 
+/**
+ * <p>
+ * Middleware is a class responsible for filtering out, and acting on requests,
+ * before they are handled by the appropriate views. They can be used to redirect
+ * non-authenticated users, or make sure a requested object exists in the database
+ * (and much, much more)
+ * </p>
+ * <p>
+ * Middleware is lined up in a sort of chain, by using a special queue.
+ * If a middleware breaks the queue, the request will not be served by the view.
+ * ( Middleware can redirect requests to other views without continuing the chain )
+ * </p>
+ * <p>
+ * <h1>Creation</h1></br>
+ * You just have to extend {@link Middleware}. Your class must have a public no-args
+ * constructor
+ * </br>
+ * <pre>
+ * public class ExampleMiddleware extends Middleware
+ * {
+ *     {@literal @}Override
+ *      public void handle(Request request, MiddlewareQueue queue)
+ *      {
+ *          if ( Foo.Bar( request ) )
+ *          {
+ *              // Here we choose to break the chain, and redirect to another view
+ *              request.internalRedirect( "other/path" );
+ *          } else
+ *          {
+ *              // This passes the request to the rest of the middleware chain
+ *              queue.handle( request );
+ *          }
+ *      }
+ * }
+ * </pre>
+ * </p>
+ * <p>
+ * <h1>Registration</h1></br>
+ * You can register your Middleware class to a RequestHandler (such as {@link SimpleRequestHandler} or
+ * ({@link com.plotsquared.iserver.views.View}) by doing:
+ * <pre>
+ * requestHandler.getMiddlewareQueuePopulator().add( ExampleMiddleware.class );
+ * </pre>
+ * Or add it to your {@literal @}{@link com.plotsquared.iserver.views.decl.ViewMatcher} like this:
+ * <pre>
+ * {@literal @}ViewMatcher(filter = "your/filter", cache = false, name = "Identifier", middlewares = { YourMiddleware.class }
+ * </pre>
+ * </p>
+ * <p>
+ * <h1>Redirects</h1></br>
+ * Middlewares can redirect a request to another view
+ * <pre>
+ * {@literal @}Override
+ *  public void handle(Request request, MiddlewareQueue queue)
+ *  {
+ *     request.internalRedirect("new/request/query");
+ *  }
+ * </pre>
+ * </p>
+ * <p>
+ * <h1>Change response generator</h1></br>
+ * RequestHandlers has a special system called Alternate outcomes, which means that they can
+ * specify other methods than the ordinary handling methods. Your middleware can tell
+ * IntellectualServer to use those methods
+ * <pre>
+ * // Inside your RequestHandler constructor
+ * registerAlternateOutcome( "identifier", "methodName" );
+ *
+ * // Method declaration
+ * public void methodName(Request request, Response response)
+ * {
+ *      // Handle the request
+ * }
+ *
+ *  // Your middleware
+ * {@literal @}Override
+ *  public void handle(Request request, MiddlewareQueue queue)
+ *  {
+ *      request.useAlternateOutcome( "methodName" );
+ *      queue.handle( request );
+ *  }
+ * </pre>
+ * </p>
+ *
+ * @see AuthenticationRequiredMiddleware
+ * @see DebugMiddleware
+ */
 public abstract class Middleware
 {
 
