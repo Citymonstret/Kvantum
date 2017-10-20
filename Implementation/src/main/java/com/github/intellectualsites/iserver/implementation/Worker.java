449c6b6c5e68e60dca7f85ef4622f590d4dbe415
Sauilitired <citymonstret@hotmail.com>
2017-10-20 14:46:06 +0200

    Move ReusableGzipOutputStream into separate file

--- Implementation/src/main/java/com/github/intellectualsites/iserver/implementation/Worker.java
+++ Implementation/src/main/java/com/github/intellectualsites/iserver/implementation/Worker.java
@@ -44,14 +44,14 @@ import com.github.intellectualsites.iserver.implementation.error.IntellectualSer
 import org.apache.commons.lang3.ArrayUtils;
 import sun.misc.BASE64Encoder;
 
-import java.io.*;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.*;
-import java.util.zip.CRC32;
-import java.util.zip.Deflater;
-import java.util.zip.DeflaterOutputStream;
 
 /**
  * This is the worker that is responsible for nearly everything.
@@ -477,120 +477,6 @@ final class Worker extends AutoCloseable
         messageDigestMd5.update( input );
         // Now encode it, yay
         return encoder.encode( messageDigestMd5.digest() );
-    }
-
-    /**
-     * Borrowed from https://github.com/oakes/Nightweb/
-     */
-    private static class ReusableGzipOutputStream extends DeflaterOutputStream
-    {
-
-        private static final byte[] HEADER = new byte[]{
-                (byte) 0x1F, (byte) 0x8b, // magic bytes
-                0x08,                   // compression format == DEFLATE
-                0x00,                   // flags (NOT using CRC16, filename, etc)
-                0x00, 0x00, 0x00, 0x00, // no modification time available (don't leak this!)
-                0x02,                   // maximum compression
-                (byte) 0xFF              // unknown creator OS (!!!)
-        };
-        private final ByteArrayOutputStream bufferStream;
-        private final CRC32 crc32;
-        private boolean headerWritten;
-        private long writtenSize;
-        private boolean written = false;
-
-        private ReusableGzipOutputStream()
-        {
-            super( new ByteArrayOutputStream(), new Deflater( 9, true ) );
-            this.crc32 = new CRC32();
-            this.bufferStream = (ByteArrayOutputStream) out;
-        }
-
-        private void reset()
-        {
-            if ( this.written )
-            {
-                this.def.reset();
-                this.crc32.reset();
-                this.writtenSize = 0;
-                this.headerWritten = false;
-                this.bufferStream.reset();
-                this.def.setLevel( Deflater.BEST_SPEED );
-                this.written = false;
-            }
-        }
-
-        private byte[] getData()
-        {
-            return this.bufferStream.toByteArray();
-        }
-
-        private void ensureWritten() throws IOException
-        {
-            if ( headerWritten )
-            {
-                return;
-            }
-            this.out.write( HEADER );
-            this.headerWritten = true;
-        }
-
-        private void writeFooter() throws IOException
-        {
-            final long crcVal = this.crc32.getValue();
-            out.write( (int) ( crcVal & 0xFF ) );
-            out.write( (int) ( ( crcVal >>> 8 ) & 0xFF ) );
-            out.write( (int) ( ( crcVal >>> 16 ) & 0xFF ) );
-            out.write( (int) ( ( crcVal >>> 24 ) & 0xFF ) );
-
-            final long sizeVal = this.writtenSize;
-            out.write( (int) ( sizeVal & 0xFF ) );
-            out.write( (int) ( ( sizeVal >>> 8 ) & 0xFF ) );
-            out.write( (int) ( ( sizeVal >>> 16 ) & 0xFF ) );
-            out.write( (int) ( ( sizeVal >>> 24 ) & 0xFF ) );
-            out.flush();
-        }
-
-        @Override
-        public void close() throws IOException
-        {
-            finish();
-            super.close();
-        }
-
-        @Override
-        public void finish() throws IOException
-        {
-            ensureWritten();
-            super.finish();
-            writeFooter();
-        }
-
-        @Override
-        public void write(final int b) throws IOException
-        {
-            this.written = true;
-            this.ensureWritten();
-            this.crc32.update( b );
-            this.writtenSize++;
-            super.write( b );
-        }
-
-        @Override
-        public void write(final byte[] b) throws IOException
-        {
-            write( b, 0, b.length );
-        }
-
-        @Override
-        public void write(final byte[] buf, final int off, final int len) throws IOException
-        {
-            this.written = true;
-            this.ensureWritten();
-            this.crc32.update( buf, off, len );
-            this.writtenSize += len;
-            super.write( buf, off, len );
-        }
     }
 
 }
