84e8fdcb24e882962bd7eb3f82b5314f94a5fe5c
Alexander Söderberg <citymonstret@hotmail.com>
2017-11-04 13:30:56 +0100

    Update HelloWorld example

--- examples/HelloWorld.java
+++ examples/HelloWorld.java
@@ -1,24 +1,25 @@
 package your.package;
 
-import com.plotsquared.iserver.DefaultLogWrapper;
-import com.plotsquared.iserver.IntellectualServerMain;
-import com.plotsquared.iserver.api.core.IntellectualServer;
-import com.plotsquared.iserver.api.util.RequestManager;
+import com.github.intellectualsites.iserver.api.core.IntellectualServer;
+import com.github.intellectualsites.iserver.api.util.RequestManager;
 
 import java.io.File;
 import java.util.Optional;
+
 
 public class HelloWorld
 {
 
     public static void main(final String[] args)
     {
-        final Optional<IntellectualServer> serverOptional = IntellectualServerMain.create(
-                /* standalone application? */ true,
-                /* working folder */ new File( "./HelloWorld" ),
-                /* log manager */ new DefaultLogWrapper(),
-                /* request router */ new RequestManager()
-        );
+        final ServerContext serverContext = ServerContext.builder()
+                .standalone( true )
+                .coreFolder( new File( "./HelloWorld" ) )
+                .logWrapper( new DefaultLogWrapper() )
+                .router( RequestManager.builder().build() ).build();
+
+        final Optional<IntellectualServer> serverOptional = serverContext.create();
+
         if ( !serverOptional.isPresent() )
         {
             System.out.println( "Failed to start the server... Check terminal for information!" );
