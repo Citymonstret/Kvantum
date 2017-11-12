636c8a4599a00e951f4feb10160e3980f649438c
Alexander Söderberg <citymonstret@hotmail.com>
2017-11-12 13:48:54 +0100

    Add tests and remove unused json-io dependency

--- ServerAPI/src/main/java/com/github/intellectualsites/kvantum/api/orm/KvantumObjectParserResult.java
+++ ServerAPI/src/main/java/com/github/intellectualsites/kvantum/api/orm/KvantumObjectParserResult.java
@@ -1,3 +1,21 @@
+/*
+ * Kvantum is a web server, written entirely in the Java language.
+ * Copyright (C) 2017 IntellectualSites
+ *
+ * This program is free software; you can redistribute it andor modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
 package com.github.intellectualsites.kvantum.api.orm;
 
 import com.github.intellectualsites.kvantum.api.orm.annotations.KvantumField;
@@ -9,6 +27,7 @@ import lombok.*;
 @RequiredArgsConstructor( access = AccessLevel.PACKAGE )
 final public class KvantumObjectParserResult<T>
 {
+
     private final T parsedObject;
     private final boolean success;
     private KvantumObjectParserError error;
@@ -16,6 +35,7 @@ final public class KvantumObjectParserResult<T>
     @AllArgsConstructor( access = AccessLevel.PACKAGE )
     public static class KvantumObjectParserCouldNotParse extends KvantumObjectParserError
     {
+
         @Getter
         private final KvantumField missingField;
 
@@ -48,6 +68,7 @@ final public class KvantumObjectParserResult<T>
     @AllArgsConstructor( access = AccessLevel.PACKAGE )
     public static class KvantumObjectParserMissingParameter extends KvantumObjectParserError
     {
+
         @Getter
         private final KvantumField missingField;
 
