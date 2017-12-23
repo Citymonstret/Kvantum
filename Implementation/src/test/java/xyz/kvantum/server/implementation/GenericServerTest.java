c86f896c3f6dedb60b7fb4e679f820cb0aa90d45
Alexander Söderberg <citymonstret@hotmail.com>
2017-12-23 19:33:43 +0100

    FIXES: Minor fixes. Remove unused dependencies.

--- Implementation/src/test/java/xyz/kvantum/server/implementation/GenericServerTest.java
+++ Implementation/src/test/java/xyz/kvantum/server/implementation/GenericServerTest.java
@@ -1,4 +1,9 @@
 /*
+ *    _  __                     _
+ *    | |/ /__   __ __ _  _ __  | |_  _   _  _ __ ___
+ *    | ' / \ \ / // _` || '_ \ | __|| | | || '_ ` _ \
+ *    | . \  \ V /| (_| || | | || |_ | |_| || | | | | |
+ *    |_|\_\  \_/  \__,_||_| |_| \__| \__,_||_| |_| |_|
  *
  *    Copyright (C) 2017 IntellectualSites
  *
@@ -14,24 +19,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package xyz.kvantum.server.implementation;/*
- * Kvantum is a web server, written entirely in the Java language.
- * Copyright (C) 2017 IntellectualSites
- *
- * This program is free software; you can redistribute it andor modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- */
+package xyz.kvantum.server.implementation;
 
 import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.AfterAll;
